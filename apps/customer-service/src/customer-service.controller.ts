6a2ded02c57fc8bb7587edd01b4d2349266eaf0b
atiqbitstream <atiq.bitstream@gmail.com>
2025-03-22 18:00:57 +0500

    handle customer creation and emit 'customer.created' event via Kafka

--- apps/customer-service/src/customer-service.controller.ts
+++ apps/customer-service/src/customer-service.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Logger, Post } from '@nestjs/common';
 import { CustomerServiceService } from './customer-service.service';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Controller()
 export class CustomerServiceController {
-  constructor(private readonly customerServiceService: CustomerServiceService) {}
+  constructor(@Inject('KAFKA_SERVICE') private readonly kafkaClient:ClientKafka) {}
 
-  @Get()
-  getHello(): string {
-    return this.customerServiceService.getHello();
-  }
+  private readonly logger = new Logger(CustomerServiceController.name);
+
+@Post()
+createCustomer(@Body() data: {id:string;name:string;email:string})
+{
+  this.logger.log(`Emitting customer created event : ${JSON.stringify(data)}`);
+  this.kafkaClient.emit('customer.created',data);
+  return {message: 'customer created', data}
+}
 }
