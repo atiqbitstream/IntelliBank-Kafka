9754d1609522461618229aa439980dd00666cef8
atiqbitstream <atiq.bitstream@gmail.com>
2025-03-23 10:12:00 +0500

    Implement TransactionController with Kafka integration and transaction processing logic

--- apps/transaction-service/src/transaction-service.controller.ts
+++ apps/transaction-service/src/transaction-service.controller.ts
@@ -1,12 +1,57 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Logger, Post } from '@nestjs/common';
 import { TransactionServiceService } from './transaction-service.service';
+import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class TransactionServiceController {
-  constructor(private readonly transactionServiceService: TransactionServiceService) {}
 
-  @Get()
-  getHello(): string {
-    return this.transactionServiceService.getHello();
+  private readonly logger = new Logger(TransactionServiceController.name)
+
+  constructor(@Inject('KAFKA_SERVICE') private readonly kafkaClient:ClientKafka,
+  private readonly transactionService: TransactionServiceService
+) 
+{}
+
+  @Post('transactions')
+  initiateTransaction(@Body() data: {from:string; to:string; amount: number, transactionId:string})
+  {
+    this.kafkaClient.emit('transaction.requested',data);
+  }
+
+  @MessagePattern('transaction.requested')
+  async processTransaction(@Payload() data: {
+    from:string,
+    to:string,
+    amount:number,
+    transactionId: string
+  })
+  {
+    this.logger.log(`Received transaction request: ${JSON.stringify(data)}`);
+
+    try{
+      const result = await this.transactionService.processTransaction(data);
+
+      this.kafkaClient.emit('transaction.processed',{
+        key:data.transactionId,
+        value:result,
+      });
+
+      if(!result.success)
+      {
+        this.kafkaClient.emit('transaction failed',{
+          key: data.transactionId,
+          value: result
+        });
+      }
+    }catch(error){
+      this.logger.error(`Transaction processing error: ${error.message}`);
+      this.kafkaClient.emit('transaction error',{
+        key: data.transactionId,
+        value: {
+          ...data,
+          error:error.message,
+        }
+      })
+    }
   }
 }
