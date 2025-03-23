b42e81e373e4ed9ea090b9a7e96ca292c00c4550
atiqbitstream <atiq.bitstream@gmail.com>
2025-03-23 10:20:41 +0500

    Implement NotificationService with dummy welcome email sending functionality

--- apps/notification-service/src/notification-service.service.ts
+++ apps/notification-service/src/notification-service.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class NotificationServiceService {
-  getHello(): string {
-    return 'Hello World!';
+ 
+  private readonly logger = new Logger(NotificationServiceService.name);
+
+  sendWelcomeEmail(email:string)
+  {
+    this.logger.log(`Ending welcome email to ${email}`);
+
+    return {status:'success', message: `Welcome email sent to ${email}`};
   }
+
+  
 }
